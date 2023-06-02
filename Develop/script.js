ca59025234669e2153e332600db496c23cce626d
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-01 20:27:09 -0400

    added comments as I puzzled things out. Things were a bit too cluttered after that, so I'm putting them into a new branch labelled withComments so I can have a version with my thought process and a version without

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 // Assignment code here
 
+// ATTN GRADER: I put lots of notes in here for myself as I puzzled things out. I'll find a way to clean that up for readability later.
+
 // VARIABLES
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -37,11 +39,15 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  // document.querySelector was in the starter code. It's new to me, so I asked ChatGPT to break things down for me.
+  // document is an object which represents the HTML document loaded in the web browser. JD said we'll learn about objects tomorrow I think.
+  // querySelector is a method that lets you select and retrieve the first element in the document that matches the specified selector - or null if there is no match found
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  // This one searches the HTML for the first id="password" it finds.
+  // Okay so: this sets the variable passwordText = line 22 of index.html, which is id="password"
 
   // My code:
+  // variable for combined characters like we did in class
   var combinedChars = [];
 
   var includeLowercase = choices[1];
@@ -49,6 +55,7 @@ function writePassword() {
   var includeNumbers = choices[3];
   var includeSpecials = choices[4];
 
+  // concatenates things based on user choices
   if (includeLowercase) {
     combinedChars = combinedChars.concat(lowercase);
   }
@@ -62,6 +69,7 @@ function writePassword() {
     combinedChars = combinedChars.concat(specials);
   }
 
+  // for loop to generate the password - same as we used in class
   for (var count = 0; count < choices[0]; count++) {
     var random = Math.random();
     var randomIndex = Math.floor(random * combinedChars.length);
@@ -69,7 +77,12 @@ function writePassword() {
     password += combinedChars[randomIndex];
   }
 
-  return password
+  // Added return password because it's what we used in class
+  // BUT I think this is when we need this part of the starter code to be used, because this puts the variable password into the text box. 
+  // ChatGPT explained that passwordText is a textarea element we obtained using the queryselector earlier
+  passwordText.value = password;
+  // variable passwordText was previously set to represent line 22 of index.html, and now we are making the value of that line (#password) equal or newly generated password.
+
 }
 
 // Add event listener to generate button
